7a7094e72c78f2d01d1d359b5a332feb0abbe283
satendra gangwar <101309019+satendragangwar@users.noreply.github.com>
2023-12-16 15:18:40 +0530

    Update Header.jsx

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
       <h4>Hello I'm</h4>
       <h1>Satendra gangwar</h1>
       <h5 className="text-light">Fullstack Developer</h5>
-      <CTA/>
+{/*       <CTA/> */}
       <HeaderSocials/>
       <div className="me">
         <img src={ME} alt="me" />
